85a06e1336f2e7548702b9ca6796d4e33b0afe14
Mathew D <mtda223@uky.edu>
2024-06-24 16:35:01 -0400

    SP CSS and Minor Cosmetic Tweaks

--- app/static/js/alerts.js
+++ app/static/js/alerts.js
@@ -20,17 +20,6 @@ export function showAlert(alertMsg, alertType)
         // <button type>: Creates a button to manually close the alert
         // <span>: Creates the (x)
     )
-    /* Previous functionality, no longer required, but kept here in case we want this again later
-
-    $("#alert").fadeTo(10000, 500).slideUp(1000, function()
-        // fadeTo(10000, 500); After 10 seconds, fade to 50% opacity
-        // slideUp(1000, function(){...}):
-        {
-            $("#alert").slideUp(1000);  // Alert Div shrinks upward in 1 second
-            $("#alert").alert('close')  // Closes the Alert
-        });
-    */
-    $('html,body').animate({scrollTop:0},'slow')    // Snaps the Page to the Top (Presumably to see the alert)
 }
 
 /*/////////////////////////////////////
